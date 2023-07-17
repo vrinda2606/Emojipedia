e31cffeed22c29a1581663c4f3b0e0c3f757a6b1
vrinda Sharma <vrinda26sharma@gmail.com>
2023-07-17 20:46:17 +0530

    intial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,28 @@
 import React from "react";
-import Card from "./Card";
-import contacts from "../contact";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
-
-function createCard(contact){
-  return <Card 
-  key={contact.id}
-    name={contact.name}
-    img={contact.imgURL}
-    tel={contact.tel}
-    email={contact.email}
+function createEntry(emojiTerm){
+  return <Entry 
+  key={emojiTerm.id}
+  emoji={emojiTerm.emoji} 
+  name={emojiTerm.name}
+  description={emojiTerm.meaning}
   />
 }
 
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
+      <h1>
+        <span>Emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(createEntry)}
+      </dl>
     </div>
   );
 }
-/*
-In contacts form either use 
-  <h1>...</h1> {contacts.map(createCard)}
-
-  OR 
-
-  <h1>...</h1> 
-      <Card 
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card 
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card 
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
-*/
 
 export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,24 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "🥰",
+      name: "Smiling face with smiling eyes and three hearts",
+      meaning:
+        "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart."
+    },
+    {
+      id: 2,
+      emoji: "😉",
+      name: "Winking Face",
+      meaning:
+        "Smiley blinks mischievously with one eye. Expression of humor, has been pulling a practical joke, is flirting with you or has an ulterior motive. The wink weakens the message: do not take it seriously, regard it as humorous.."
+    },
+    {
+      id: 3,
+      emoji: "😂",
+      name: "Face With Tears of Joy",
+      meaning:
+        "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself.“. Stands for „rolling on the floor, laughing“."
+    }
+  ];
+  export default emojipedia;
